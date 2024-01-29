df11abf5f093a6cca02faa51f3d67151c6ad5fc1
jimmis <jimmis.imp@gmail.com>
2024-01-29 15:55:24 -0500

    Adding user

--- sql.py
+++ sql.py
@@ -1,1 +1,26 @@
-print("Hello")
+from cryptography.fernet import Fernet
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# encryption_key = b'5gP6PJiet87ZyOYda17lzFemrpK3bGk3f5zLm5nZsBU='
+# cipher_suite = Fernet(encryption_key)
+# openai_api_key = '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'
+# encrypted_api_key = cipher_suite.encrypt(openai_api_key.encode())
+# print(encrypted_api_key)
+
+print("Starting db update")
+
+encrypted_key = b'gAAAAABluA62XWiJH-TiyQxUEeLP3t6w2QHcWt_p6asszSGP7EkCV987G-woX7ZUsAjP_lqUCm09XtKqTC3K2vXfw5sYpQP4PsWZBkgXxTAo3PwlwPUSgoKRmgzlRcV3oKyj5kgc-c_4VWPQHJxbYQJg-MkbAhIZrg=='
+
+password = "ox35vl9ys3jy"
+password_hash = generate_password_hash(password)
+
+conn = sqlite3.connect('items.db')
+# Execute the update with parameter substitution
+conn.execute("INSERT INTO users (username,password,openai_api_key) VALUES (?, ?, ?)", ("chefSteph",generate_password_hash,encrypted_key))
+conn.commit()
+
+# Close the connection
+conn.close()
+
+print("chefSteph",generate_password_hash,encrypted_key)
