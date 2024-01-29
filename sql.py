c1d0fa748c2c34f831e8b6a50fa46cf7a59fb80f
jimmis <jimmis.imp@gmail.com>
2024-01-29 16:11:39 -0500

    Update username

--- sql.py
+++ sql.py
@@ -8,19 +8,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # encrypted_api_key = cipher_suite.encrypt(openai_api_key.encode())
 # print(encrypted_api_key)
 
-print("Starting db update")
+# print("Starting db update")
 
-encrypted_key = b'gAAAAABluA62XWiJH-TiyQxUEeLP3t6w2QHcWt_p6asszSGP7EkCV987G-woX7ZUsAjP_lqUCm09XtKqTC3K2vXfw5sYpQP4PsWZBkgXxTAo3PwlwPUSgoKRmgzlRcV3oKyj5kgc-c_4VWPQHJxbYQJg-MkbAhIZrg=='
+# encrypted_key = b'gAAAAABluA62XWiJH-TiyQxUEeLP3t6w2QHcWt_p6asszSGP7EkCV987G-woX7ZUsAjP_lqUCm09XtKqTC3K2vXfw5sYpQP4PsWZBkgXxTAo3PwlwPUSgoKRmgzlRcV3oKyj5kgc-c_4VWPQHJxbYQJg-MkbAhIZrg=='
 
-password = "ox35vl9ys3jy"
-password_hash = generate_password_hash(password)
+# password = "ox35vl9ys3jy"
+# password_hash = generate_password_hash(password)
 
+# conn = sqlite3.connect('items.db')
+# # Execute the update with parameter substitution
+# conn.execute("INSERT INTO users (username,password_hash,openai_api_key) VALUES (?, ?, ?)", ("chefStep",password_hash,encrypted_key))
+# conn.commit()
+
+# # Close the connection
+# conn.close()
+
+# print("chefSteph",generate_password_hash,encrypted_key)
 conn = sqlite3.connect('items.db')
-# Execute the update with parameter substitution
-conn.execute("INSERT INTO users (username,password_hash,openai_api_key) VALUES (?, ?, ?)", ("chefSteph",password_hash,encrypted_key))
+conn.execute("UPDATE users SET username = ? WHERE username = chefSteph"("chefStef"))
 conn.commit()
 
 # Close the connection
 conn.close()
-
-print("chefSteph",generate_password_hash,encrypted_key)
