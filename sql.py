26c6ddf4fa8b2cb1069b59d2db6db6905803a7c9
jimmis <jimmis.imp@gmail.com>
2024-02-11 14:08:47 -0500

    Styling

--- sql.py
+++ sql.py
@@ -3,21 +3,8 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 print("Starting db update")
 
-conn = sqlite3.connect('items.db')
-
-create_table_sql = """
-CREATE TABLE IF NOT EXISTS deleted (
-    delete_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    user_id INTEGER NOT NULL
-);
-"""
-
+# conn = sqlite3.connect('items.db')
 # Execute the SQL command
-conn.execute(create_table_sql)
-conn.commit()
-conn.close()
-
 
 # encryption_key = b'5gP6PJiet87ZyOYda17lzFemrpK3bGk3f5zLm5nZsBU='
 # cipher_suite = Fernet(encryption_key)
